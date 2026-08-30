53669796c1c7e8124af78f10319b1b3b5d01a250
buyer69seller <99603615+buyer69seller@users.noreply.github.com>
2026-08-30 21:20:30 +0700

    Update __init__.py

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -2,5 +2,6 @@
 from .reward_service import RewardService
 from .loadout_service import LoadoutService
 from .marketplace_service import MarketplaceService
+from .auth_service import AuthService
 
-__all__ = ["RewardService", "LoadoutService", "MarketplaceService"]
+__all__ = ["RewardService", "LoadoutService", "MarketplaceService", "AuthService"]
