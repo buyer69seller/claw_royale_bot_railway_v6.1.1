85f190918374b675541bed72cf3360977dd6f62a
buyer69seller <99603615+buyer69seller@users.noreply.github.com>
2026-08-31 21:20:16 +0700

    Update __init__.py

--- src/strategy/__init__.py
+++ src/strategy/__init__.py
@@ -1,20 +1,16 @@
 # src/strategy/__init__.py
-"""Strategy Module - Hybrid AI + Scan & Clear"""
+"""Strategy module - Heuristic strategy engine"""
 
 from .engine import StrategyEngine
-from .evaluators import *
-from .scan_clear import ScanClearStrategy
-from .hybrid_strategy import HybridStrategy  # <-- TAMBAHKAN (jika file ada)
-
+from .evaluators import (
+    num, hp, max_hp, alive,
+    heal_score, combat_score, loot_score,
+    interact_score, explore_score, move_score,
+    get_pack_strategy_modifier, apply_pack_modifiers
+)
 
 __all__ = [
-    # Main Strategy Engine
     "StrategyEngine",
-    "HybridStrategy",  # <-- TAMBAHKAN
-    # Scan & Clear Strategy
-    "ScanClearStrategy",
-    
-    # Evaluators
     "num",
     "hp",
     "max_hp",
